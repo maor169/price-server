60adfa2380345b95319edb6d1202e8e178d45602
maor169 <maor169@gmail.com>
2026-05-02 20:37:08 +0300

    fix missing axios require

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
 const PRODUCTS = require('./products');
 const CONTROLLED = require('./controlled');
 
